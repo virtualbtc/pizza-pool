a6af656cb6e26ba7a8971fd710c6fd68e41bec0a
Hanul <hanul@hanul.me>
2021-08-28 22:36:59 +0900

    dev pizza pool

--- contracts/PizzaPool.sol
+++ contracts/PizzaPool.sol
@@ -9,27 +9,24 @@ contract PizzaPool is IPizzaPool {
     IVirtualBitcoin private vbtc;
     
     struct Pool {
+        address owner;
         uint256 pizzaId;
-        address owner;
-        uint256 sliceAllowed;
-        uint256 sliceAllowedPrice;
-        uint256 sliceOwned;
         uint256 currentBalance;
-        uint256 sharePerSlice;
-        uint256 ownerClaimed;
-        uint256 fee;
+        uint256 pointsPerShare;
     }
-    Pool[] public pools;
+    Pool[] override public pools;
     mapping(uint256 => uint256) public pizzaToPool;
+    mapping(uint256 => mapping(address => uint256)) public slices;
+    mapping(uint256 => mapping(address => int256)) internal pointsCorrection;
+    mapping(uint256 => mapping(address => uint256)) internal claimed;
     
-    struct Slice {
-        address owner;
-        address amount;
+    struct Sale {
+        address seller;
+        uint256 poolId;
+        uint256 slice;
         uint256 price;
-        uint256 claimed;
     }
-    mapping(uint256 => Slice[]) public slices;
-    mapping(uint256 => mapping(address => uint256)) public ownerToSlice;
+    Sale[] override public sales;
 
     constructor(IVirtualBitcoin _vbtc) {
         vbtc = _vbtc;
@@ -39,32 +36,25 @@ contract PizzaPool is IPizzaPool {
         return pools.length;
     }
 
-    function createPool(uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee) override external returns (uint256) {
-        
-        uint256 price = vbtc.pizzaPrice(power);
-        require(sliceAllowed <= price);
-        
-        vbtc.transferFrom(msg.sender, address(this), price);
+    function createPool(uint256 power) override external returns (uint256) {
+
+        uint256 slice = vbtc.pizzaPrice(power);
+        vbtc.transferFrom(msg.sender, address(this), slice);
 
         uint256 pizzaId = vbtc.buyPizza(power);
         uint256 poolId = pools.length;
 
         pools.push(Pool({
-            pizzaId: pizzaId,
             owner: msg.sender,
-            sliceAllowed: sliceAllowed,
-            sliceAllowedPrice: sliceAllowedPrice,
-            sliceOwned: 0,
+            pizzaId: pizzaId,
             currentBalance: 0,
-            sharePerSlice: 0,
-            ownerClaimed: 0,
-            fee: fee
+            pointsPerShare: 0
         }));
 
         pizzaToPool[pizzaId] = poolId;
 
-        slices[poolId].push(); // add empty slice
-        emit CreatePool(msg.sender, poolId, power, sliceAllowed, sliceAllowedPrice, fee);
+        slices[poolId][msg.sender] = slice;
+        emit CreatePool(poolId, msg.sender, pizzaId, power);
         return poolId;
     }
     
@@ -73,120 +63,143 @@ contract PizzaPool is IPizzaPool {
         require(balance > 0);
         uint256 value = balance - pool.currentBalance;
         if (value > 0) {
-            pool.sharePerSlice += value * 1e8 / vbtc.pizzaPrice(pool.power);
+            pool.pointsPerShare += value * 1e8 / vbtc.pizzaPrice(vbtc.powerOf(pool.pizzaId));
         }
         pool.currentBalance = balance;
     }
 
-    function changePool(uint256 poolId, uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee) override external {
-        
-        Pool storage pool = pools[poolId];
-        
-        require(pool.owner == msg.sender);
-        require(sliceAllowed <= pool.sliceOwned && sliceAllowed <= vbtc.pizzaPrice(power));
-        require(fee <= pool.fee);
+    function changePool(uint256 poolId, uint256 power) override external {
 
-        updateBalance(pool);
-        pool.sliceAllowed = sliceAllowed;
-        pool.sliceAllowedPrice = sliceAllowedPrice;
-        pool.fee = fee;
+        Pool storage pool = pools[poolId];
+        require(pool.owner == msg.sender);
         
         uint256 pizzaId = pool.pizzaId;
         uint256 currentPower = vbtc.powerOf(pizzaId);
+
         if (currentPower < power) { // upgrade
-            uint256 price = vbtc.pizzaPrice(power - currentPower);
-            vbtc.transferFrom(msg.sender, address(this), price);
-        } else if (currentPower > power) { // downgrade
-            uint256 price = vbtc.pizzaPrice(currentPower - power);
-            vbtc.transfer(msg.sender, price);
+            
+            uint256 slice = vbtc.pizzaPrice(power - currentPower);
+            slices[poolId][msg.sender] += slice;
+
+            vbtc.transferFrom(msg.sender, address(this), slice);
+            updateBalance(pool);
+            vbtc.changePizza(pizzaId, power);
+            
+            emit ChangePool(poolId, power);
         }
         
-        vbtc.changePizza(pizzaId, power);
-        emit ChangePool(msg.sender, poolId, power, sliceAllowed, sliceAllowedPrice, fee);
+        else if (currentPower > power) { // downgrade
+            
+            uint256 slice = vbtc.pizzaPrice(currentPower - power);
+            slices[poolId][msg.sender] -= slice;
+
+            updateBalance(pool);
+            vbtc.changePizza(pizzaId, power);
+            vbtc.transfer(msg.sender, slice);
+            
+            emit ChangePool(poolId, power);
+        }
     }
 
     function deletePool(uint256 poolId) override external {
-        Pool memory pool = pools[poolId];
+
+        Pool storage pool = pools[poolId];
         require(pool.owner == msg.sender);
-        require(pool.sliceOwned == 0);
         
         uint256 pizzaId = pool.pizzaId;
-        uint256 balance = vbtc.subsidyOf(pizzaId) + vbtc.pizzaPrice(vbtc.powerOf(pizzaId));
+        uint256 subsidy = vbtc.subsidyOf(pizzaId);
+        uint256 slice = vbtc.pizzaPrice(vbtc.powerOf(pizzaId));
+        
+        slices[poolId][msg.sender] -= slice;
+
         vbtc.sellPizza(pizzaId);
-        vbtc.transfer(msg.sender, balance);
+        vbtc.transfer(msg.sender, subsidy + slice);
 
         delete pools[poolId];
         delete pizzaToPool[pizzaId];
 
-        emit DeletePool(msg.sender, poolId);
-    }
-    
-    function sliceCount(uint256 poolId) override external view returns (uint256) {
-        return slices[poolId].length;
+        emit DeletePool(poolId);
     }
 
-    function joinPool(uint256 poolId, uint256 amount) override external {
-        require(ownerToSlice[poolId][msg.sender] == 0);
+    function saleCount() override external view returns (uint256) {
+        return sales.length;
+    }
 
-        Pool storage pool = pools[poolId];
-        require(pool.owner != address(0));
-        require(pool.sliceAllowed - pool.sliceOwned >= amount);
-
-        uint256 price = amount * pool.sliceAllowedPrice / pool.sliceAllowed;
-
-        Slice[] storage ss = slices[poolId];
-        uint256 sliceId = ss.length;
-        ss.push(Slice({
-            owner: msg.sender,
-            amount: amount,
-            price: price,
-            claimed: 0
+    function sell(uint256 poolId, uint256 slice, uint256 price) override external returns (uint256) {
+        
+        uint256 saleId = sales.length;
+        sales.push(Sale({
+            seller: msg.sender,
+            poolId: poolId,
+            slice: slice,
+            price: price
         }));
-        
-        pool.sliceOwned += amount;
-        ownerToSlice[poolId][msg.sender] = sliceId;
 
-        vbtc.transferFrom(msg.sender, pool.owner, price);
-        emit JoinPool(msg.sender, poolId, amount);
+        emit Sell(saleId, msg.sender, poolId, slice, price);
+        return saleId;
     }
 
-    function changeSlice(uint256 poolId, uint256 amount) override external {
-        
-        uint256 sliceId = ownerToSlice[poolId][msg.sender];
-        require(sliceId != 0);
-        Slice storage slice = slices[poolId][sliceId];
-        
-        uint256 currentAmount = slice.amount;
-        require(currentAmount != amount);
+    function removeSale(uint256 saleId) internal {
+        delete sales[saleId];
+        emit RemoveSale(saleId);
+    }
 
-        claimSlice(poolId);
-
+    function buy(uint256 saleId, uint256 slice) override external {
+        
+        Sale storage sale = sales[saleId];
+        uint256 poolId = sale.poolId;
+        uint256 price = slice * sale.price / sale.slice;
+        sale.slice -= slice;
+        sale.price -= price;
+        
         Pool storage pool = pools[poolId];
-        if (currentAmount < amount) { // upgrade
-            uint256 diff = amount - currentAmount;
-            pool.sliceOwned += diff;
-            uint256 price = diff * pool.sliceAllowedPrice / pool.sliceAllowed;
-            vbtc.transferFrom(msg.sender, pool.owner, price);
+        updateBalance(pool);
+        int256 correction = int256(pool.pointsPerShare * slice);
+        pointsCorrection[poolId][sale.seller] += correction;
+        pointsCorrection[poolId][msg.sender] -= correction;
+        
+        slices[poolId][sale.seller] -= slice;
+        slices[poolId][msg.sender] += slice;
+
+        vbtc.transferFrom(msg.sender, sale.seller, price);
+
+        emit Buy(saleId, msg.sender, slice);
+        if (sale.slice == 0) {
+            removeSale(saleId);
         }
     }
 
-    function exitPool(uint256 poolId) override external {
+    function cancelSale(uint256 saleId) override external {
 
-    }
-    
-    function subsidyPoolOf(uint256 poolId) override external view returns (uint256) {
+        Sale memory sale = sales[saleId];
+        require(sale.seller == msg.sender);
 
-    }
-
-    function claimPool(uint256 poolId) override external {
-
+        emit CancelSale(saleId);
+        removeSale(saleId);
     }
 
-    function subsidySliceOf(uint256 poolId) override external view returns (uint256) {
-
+    function subsidyOf(uint256 poolId) override external view returns (uint256) {
+        Pool memory pool = pools[poolId];
+        uint256 pointsPerShare = pool.pointsPerShare;
+        uint256 balance = vbtc.subsidyOf(pool.pizzaId);
+        require(balance > 0);
+        uint256 value = balance - pool.currentBalance;
+        if (value > 0) {
+            pointsPerShare += value * 1e8 / vbtc.pizzaPrice(vbtc.powerOf(pool.pizzaId));
+        }
+        return uint256(int256(pointsPerShare * slices[poolId][msg.sender]) + pointsCorrection[poolId][msg.sender]) / 1e8 - claimed[poolId][msg.sender];
     }
 
-    function claimSlice(uint256 poolId) public external {
-        
+    function mine(uint256 poolId) override external returns (uint256) {
+        Pool storage pool = pools[poolId];
+        updateBalance(pool);
+        uint256 subsidy = uint256(int256(pool.pointsPerShare * slices[poolId][msg.sender]) + pointsCorrection[poolId][msg.sender]) / 1e8 - claimed[poolId][msg.sender];
+        if (subsidy > 0) {
+            claimed[poolId][msg.sender] += subsidy;
+            emit Mine(msg.sender, poolId, subsidy);
+            vbtc.transfer(msg.sender, subsidy);
+            pool.currentBalance -= subsidy;
+        }
+        return subsidy;
     }
 }
--- contracts/interfaces/IPizzaPool.sol
+++ contracts/interfaces/IPizzaPool.sol
@@ -3,24 +3,29 @@ pragma solidity ^0.8.5;
 
 interface IPizzaPool {
     
-    event CreatePool(address indexed owner, uint256 indexed poolId, uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee);
-    event ChangePool(address indexed owner, uint256 indexed poolId, uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee);
-    event DeletePool(address indexed owner, uint256 indexed poolId);
-    
-    event JoinPool(address indexed owner, uint256 indexed poolId, uint256 amount);
+    event CreatePool(uint256 indexed poolId, address indexed owner, uint256 indexed pizzaId, uint256 power);
+    event ChangePool(uint256 indexed poolId, uint256 power);
+    event DeletePool(uint256 indexed poolId);
 
+    event Sell(uint256 indexed saleId, address indexed seller, uint256 indexed poolId, uint256 slice, uint256 price);
+    event RemoveSale(uint256 indexed saleId);
+    event Buy(uint256 indexed saleId, address indexed buyer, uint256 slice);
+    event CancelSale(uint256 indexed saleId);
+    
+    event Mine(address indexed owner, uint256 indexed poolId, uint256 subsidy);
+    
     function poolCount() external view returns (uint256);
-    function createPool(uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee) external returns (uint256);
-    function changePool(uint256 poolId, uint256 power, uint256 sliceAllowed, uint256 sliceAllowedPrice, uint256 fee) external;
+    function pools(uint256 poolId) external view returns (address owner, uint256 pizzaId, uint256 currentBalance, uint256 pointsPerShare);
+    function createPool(uint256 power) external returns (uint256);
+    function changePool(uint256 poolId, uint256 power) external;
     function deletePool(uint256 poolId) external;
-    
-    function sliceCount(uint256 poolId) external view returns (uint256);
-    function joinPool(uint256 poolId, uint256 amount) external;
-    function changeSlice(uint256 poolId, uint256 amount) external;
-    function exitPool(uint256 poolId) external;
-    
-    function subsidyPoolOf(uint256 poolId) external view returns (uint256);
-    function claimPool(uint256 poolId) external;
-    function subsidySliceOf(uint256 poolId) external view returns (uint256);
-    function claimSlice(uint256 poolId) external;
+
+    function saleCount() external view returns (uint256);
+    function sales(uint256 saleId) external view returns (address seller, uint256 poolId, uint256 slice, uint256 price);
+    function sell(uint256 poolId, uint256 slice, uint256 price) external returns (uint256);
+    function buy(uint256 saleId, uint256 slice) external;
+    function cancelSale(uint256 saleId) external;
+
+    function subsidyOf(uint256 poolId) external view returns (uint256);
+    function mine(uint256 poolId) external returns (uint256);
 }
