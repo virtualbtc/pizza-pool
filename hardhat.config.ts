50614710b90e7f252f3e8dc676dd83691decb87e
TheGreatHB <kavanaboy@gmail.com>
2021-09-03 02:47:28 +0900

    Set testing environment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,37 @@
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "dotenv/config";
-import "hardhat-typechain";
-import { HardhatUserConfig } from "hardhat/types";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.5",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
   networks: {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ''],
+      // accounts: [process.env.ADMIN || ''],
       chainId: 4,
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ''],
+      // accounts: [process.env.ADMIN || ''],
       chainId: 42,
     },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
   },
 };
 
